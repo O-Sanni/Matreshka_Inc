e1cbb029d6b1421d7a5f2bc4ae79930d872dd0ac
Olga_Sanni <dev.sannikov@gmail.com>
2020-05-31 10:07:04 -0400

    Admin's page

--- matreshka_store/src/componets/AdminsPage.js
+++ matreshka_store/src/componets/AdminsPage.js
@@ -5,7 +5,7 @@ class AdminsPage extends React.Component{
 constructor(props){
     super(props);
 
-        this.state = {russianBooks: [], russianOrders:[], subscribe:[], requests:[]};
+        this.state = {russianBooks: [], russianGifts:[], subscribe:[], requests:[]};
         this.remove = this.remove.bind(this);
 
     }
@@ -14,76 +14,102 @@ constructor(props){
   }
 
   async getGiftsInfo(){
- 
     try{
         let giftsData= await axios.get(`https://matreshka-database.herokuapp.com/book_store/v1/gifts`)
-        this.setState({russianGiftList:giftsData.data})
+        this.setState({russianGifts:giftsData.data})
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+async getBooksInfo(){
+    try{
+        let booksData= await axios.get(`https://matreshka-database.herokuapp.com/book_store/v1/books`)
+        this.setState({russianBooks:booksData.data})
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+async getSubscribe(){
+    try{
+        let subscribeData= await axios.get(`https://matreshka-database.herokuapp.com/book_store/v1/subscribe`)
+        this.setState({subscribe:subscribeData.data})
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+async getRequests(){
+    try{
+        let requestsData= await axios.get(`https://matreshka-database.herokuapp.com/book_store/v1/requests`)
+        this.setState({requests:requestsData.data})
     }
     catch(error){
         console.log(error)
     }
 }
 
-  async remove(id) {
-    await fetch(`/niecey_api/v1/employees/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then(() => {
-      let updatedEmployees = [...this.state.employees].filter(i => i.id !== id);
-      this.setState({employees: updatedEmployees});
-    });
-  }
+//   async remove(id) {
+//     await fetch(`/niecey_api/v1/employees/${id}`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json'
+//       }
+//     }).then(() => {
+//       let updatedEmployees = [...this.state.employees].filter(i => i.id !== id);
+//       this.setState({employees: updatedEmployees});
+//     });
+//   }
 
   render() {
-    const {employees, isLoading} = this.state;
+//     const {employees, isLoading} = this.state;
 
-    if (isLoading) {
-      return <p>Loading...</p>;
-    }
+//     if (isLoading) {
+//       return <p>Loading...</p>;
+//     }
 
-    const employeeList = employees.map(employee => {
-      return <tr key={employee.id}>
-        <td style={{whiteSpace: 'nowrap'}}>{employee.firstName}</td>
-        <td>{employee.lastName}</td>
-        <td>{employee.email}</td>
-        <td>
-          <ButtonGroup>
-            <Button size="sm" color="primary" tag={Link} to={"/employee/" + employee.id}>Edit</Button>
-            <Button size="sm" color="danger" onClick={() => this.remove(employee.id)}>Delete</Button>
-          </ButtonGroup>
-        </td>
-      </tr>
-    });
+//     const employeeList = employees.map(employee => {
+//       return <tr key={employee.id}>
+//         <td style={{whiteSpace: 'nowrap'}}>{employee.firstName}</td>
+//         <td>{employee.lastName}</td>
+//         <td>{employee.email}</td>
+//         <td>
+//           <ButtonGroup>
+//             <Button size="sm" color="primary" tag={Link} to={"/employee/" + employee.id}>Edit</Button>
+//             <Button size="sm" color="danger" onClick={() => this.remove(employee.id)}>Delete</Button>
+//           </ButtonGroup>
+//         </td>
+//       </tr>
+//     });
 
-    return (
-      <div>
-        <AppNavbar/>
-        <Container fluid>
-          <div className="float-right">
-            <Button color="success" tag={Link} to="/employee/new">Add Group</Button>
-          </div>
-          <h3>My Employees List</h3>
-          <Table className="mt-4">
-            <thead>
-            <tr>
-              <th width="20%">First Name</th>
-              <th width="20%">Last Name</th>
-              <th>Email</th>
-              <th width="10%">Actions</th>
-            </tr>
-            </thead>
-            <tbody>
-            {employeeList}
-            </tbody>
-          </Table>
-        </Container>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div>
+//         <AppNavbar/>
+//         <Container fluid>
+//           <div className="float-right">
+//             <Button color="success" tag={Link} to="/employee/new">Add Group</Button>
+//           </div>
+//           <h3>My Employees List</h3>
+//           <Table className="mt-4">
+//             <thead>
+//             <tr>
+//               <th width="20%">First Name</th>
+//               <th width="20%">Last Name</th>
+//               <th>Email</th>
+//               <th width="10%">Actions</th>
+//             </tr>
+//             </thead>
+//             <tbody>
+//             {employeeList}
+//             </tbody>
+//           </Table>
+//         </Container>
+//       </div>
+//     );
+//   }
+// }
 }
 }
 
